04d37c03aed45a26b3377136acda4f3f92eb5f7d
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-24 15:32:07 +0200

    M2 - converted in while

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const shop_list = [`salumi`, `pasta`, `carne`, `pesce`, `uova`, `legumi`];
 
-for (let i = 0; i < shop_list.length; i++) {
+// for (let i = 0; i < shop_list.length; i++) {
+// 	const shop_item = shop_list[i];
+// 	console.log(i, shop_item);
+// }
+
+let i = 0;
+while (i < shop_list.length) {
 	const shop_item = shop_list[i];
 	console.log(i, shop_item);
+	i++;
 }
+console.log(i);
