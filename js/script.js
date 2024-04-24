3bcb7caf7e1fdfa78fe964badf26150c1d13b9dc
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-24 15:24:40 +0200

    M1 -  array item in console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-alert(`Ciao`);
+const shop_list = [`salumi`, `pasta`, `carne`, `pesce`, `uova`, `legumi`];
+
+for (let i = 0; i < shop_list.length; i++) {
+	const shop_item = shop_list[i];
+	console.log(i, shop_item);
+}
