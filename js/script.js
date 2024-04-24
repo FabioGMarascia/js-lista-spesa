72a7bea19942f60f9292a8e3389f2570c66278b4
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-24 15:49:49 +0200

    B1 2/2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const shop_list = [`Salumi`, `Pasta`, `Carne`, `Pesce`, `Uova`, `Legumi`];
 
 // M1
+
 // for (let i = 0; i < shop_list.length; i++) {
 // 	const shop_item = shop_list[i];
 // 	console.log(i, shop_item);
 // }
 
 // M2
+
 // let i = 0;
 // while (i < shop_list.length) {
 // 	const shop_item = shop_list[i];
@@ -15,14 +17,23 @@ const shop_list = [`Salumi`, `Pasta`, `Carne`, `Pesce`, `Uova`, `Legumi`];
 // }
 // console.log(i);
 
-// M3 - B1 1/2
 const list = document.querySelector(`ul`);
+// M3 - B1 1/2
+
+// let i = 0;
+// while (i < shop_list.length) {
+// 	const shop_item = shop_list[i];
+// 	let list_element = document.createElement(`li`);
+// 	list_element.append(shop_item);
+// 	list.append(list_element);
+// 	i++;
+// }
+
+// B1 2/2
 
 let i = 0;
 while (i < shop_list.length) {
 	const shop_item = shop_list[i];
-	let list_element = document.createElement(`li`);
-	list_element.append(shop_item);
-	list.append(list_element);
+	list.innerHTML += `<li>${shop_item}</li>`;
 	i++;
 }
